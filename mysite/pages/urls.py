7d3527ead372a25b7555055b7f6ec6efd5c205dc
junes7 <2junes@naver.com>
2020-06-13 17:46:49 +0900

    django urls division study

--- mysite/pages/urls.py
+++ mysite/pages/urls.py
@@ -1,6 +1,7 @@
 # pages/urls.py
 from django.urls import path
 from . import views
+app_name="pages"
 
 urlpatterns = [
     path('index/', views.index, name="index"),
--- mysite/secondapp/views.py
+++ mysite/secondapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'secondapp/index.html')
+
+def static_example(request):
+    return render(request, 'secondapp/static_example.html')
